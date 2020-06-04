90963c7c2b30500a6c55c8d1e8714a8713aedb3f
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-06-03 23:58:14 -0300

    Add register page

---
+++ src/pages/Register/index.tsx
@@ -0,0 +1,291 @@
+import React, { useEffect, useState, ChangeEvent, useRef } from 'react';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { useHistory } from 'react-router-dom';
+import { LeafletMouseEvent } from 'leaflet';
+import * as Yup from 'yup';
+
+import api from '../../services/api';
+import Logo from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+import { toast } from 'react-toastify';
+import ibge from '../../services/ibge';
+import { FormHandles } from '@unform/core';
+import {
+  Container,
+  Link,
+  Form,
+  FieldGroup,
+  Field,
+  MapContainer,
+  Items,
+  Item,
+} from './styles';
+
+interface Point {
+  name: string;
+  email: string;
+  whatsapp: string;
+  uf: string;
+  city: string;
+}
+
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
+interface UF {
+  sigla: string;
+}
+
+interface City {
+  nome: string;
+}
+
+interface ValidationErrors {
+  name?: string;
+  email?: string;
+  whatsapp?: string;
+  uf?: string;
+  city?: string;
+}
+
+const Register: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [ufs, setUfs] = useState<string[]>([]);
+  const [cities, setCities] = useState<string[]>([]);
+
+  const history = useHistory();
+  const form_ref = useRef<FormHandles>(null);
+
+  const [selected_items, setSelectedItems] = useState<number[]>([]);
+  const [position, setPosition] = useState<[number, number]>([0, 0]);
+
+  const handleSelectUf = (event: ChangeEvent<HTMLSelectElement>) => {
+    const uf = event.target.value;
+
+    if (uf.length > 0) {
+      (async () => {
+        try {
+          const { data } = await ibge.get<City[]>(`/estados/${uf}/municipios`);
+          setCities(data.map(city => city.nome));
+        } catch (err) {
+          toast.error(
+            'Opa! Alguma coisa deu errado ao tentar carregar a lista de municípios, tente recarregar a pagina!',
+          );
+        }
+      })();
+    }
+  };
+
+  const handleMapClick = (event: LeafletMouseEvent) => {
+    setPosition([event.latlng.lat, event.latlng.lng]);
+  };
+
+  const handleSelectItem = (id: number) => {
+    if (selected_items.includes(id)) {
+      const filtered_items = selected_items.filter(item => item !== id);
+      setSelectedItems(filtered_items);
+    } else {
+      setSelectedItems([...selected_items, id]);
+    }
+  };
+
+  const handleSubmit = async ({ name, email, whatsapp, uf, city }: Point) => {
+    try {
+      const [latitude, longitude] = position;
+      const items = selected_items.join(',');
+      const point = {
+        name,
+        email,
+        whatsapp,
+        uf,
+        city,
+        latitude,
+        longitude,
+        items,
+      };
+
+      const schema = Yup.object().shape({
+        name: Yup.string()
+          .min(3, 'Digite pelo menos 3 caracteres')
+          .required('O nome da entidade é obrigatório'),
+        email: Yup.string()
+          .email('Digite um email válido')
+          .required('O email é obrigatório'),
+        whatsapp: Yup.string().required('O WhatsApp é obrigatório'),
+        uf: Yup.string()
+          .length(2, 'Digite apenas a UF do estado')
+          .required('O estado é obrigatório'),
+        city: Yup.string().required('A cidade é obrigatória'),
+      });
+
+      await schema.validate(point, { abortEarly: false });
+
+      await api.post('/points', point);
+
+      history.push('/');
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const validation_errors: ValidationErrors = {};
+
+        err.inner.forEach(error => {
+          // validation_errors[error.path] = error.message;
+        });
+
+        if (form_ref.current) {
+          form_ref.current.setErrors(validation_errors);
+        }
+      } else {
+        toast.error(
+          'Opa! Alguma coisa deu errado, tente novamente mais tarde!',
+        );
+      }
+    }
+  };
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const { latitude, longitude } = position.coords;
+      setPosition([latitude, longitude]);
+    });
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await api.get('/items');
+        setItems(data);
+      } catch (err) {
+        toast.error(
+          'Opa! Alguma coisa deu errado ao tentar carregar a lista de items para coleta, tente recarregar a pagina!',
+        );
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await ibge.get<UF[]>('/estados');
+        setUfs(data.map(uf => uf.sigla).sort());
+      } catch (err) {
+        toast.error(
+          'Opa! Alguma coisa deu errado ao tentar carregar a lista de estados, tente recarregar a pagina!',
+        );
+      }
+    })();
+  }, []);
+
+  return (
+    <Container>
+      <header>
+        <img src={Logo} alt="Ecoleta" />
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para Home
+        </Link>
+      </header>
+
+      <Form ref={form_ref} onSubmit={handleSubmit}>
+        <h1>
+          Cadastro do
+          <br /> ponto de coleta
+        </h1>
+
+        <fieldset>
+          <legend>
+            <h2>Dados</h2>
+          </legend>
+
+          <Field>
+            <label htmlFor="name">Nome da entidade</label>
+            <Input type="text" name="name" id="name" />
+          </Field>
+
+          <FieldGroup>
+            <Field>
+              <label htmlFor="email">Email</label>
+              <Input type="email" name="email" id="email" />
+            </Field>
+            <Field>
+              <label htmlFor="whatsapp">WhatsApp</label>
+              <Input type="text" name="whatsapp" id="whatsapp" />
+            </Field>
+          </FieldGroup>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Endereço</h2>
+            <span>Selecione o endereço no mapa</span>
+          </legend>
+
+          <MapContainer>
+            <Map center={position} zoom={15} onClick={handleMapClick}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              {position && <Marker position={position} />}
+            </Map>
+          </MapContainer>
+
+          <FieldGroup>
+            <Field>
+              <label htmlFor="uf">UF</label>
+              <Select name="uf" id="uf" onChange={handleSelectUf}>
+                <option value="">Selecione um estado</option>
+                {ufs.map(uf => (
+                  <option key={uf} value={uf}>
+                    {uf}
+                  </option>
+                ))}
+              </Select>
+            </Field>
+            <Field>
+              <label htmlFor="city">Cidade</label>
+              <Select name="city" id="city">
+                <option value="">Selecione uma cidade</option>
+                {cities.map(city => (
+                  <option key={city} value={city}>
+                    {city}
+                  </option>
+                ))}
+              </Select>
+            </Field>
+          </FieldGroup>
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            <h2>Ítens de coleta</h2>
+            <span>Selecione um ou mais itens abaixo</span>
+          </legend>
+
+          <Items>
+            {items.map(item => (
+              <Item
+                key={item.id.toString()}
+                onClick={() => handleSelectItem(item.id)}
+                selected={selected_items.includes(item.id)}
+              >
+                <img src={item.image_url} alt={item.title} />
+                <span>{item.title}</span>
+              </Item>
+            ))}
+          </Items>
+        </fieldset>
+
+        <button type="submit">Cadastrar ponto de coleta</button>
+      </Form>
+    </Container>
+  );
+};
+
+export default Register;
---
+++ src/pages/Register/styles.ts
@@ -0,0 +1,189 @@
+import styled, { css } from 'styled-components';
+import { Link as RouterLink } from 'react-router-dom';
+import { Form as Unform } from '@unform/web';
+
+export const Container = styled.div`
+  margin: 0px auto;
+  max-width: 1100px;
+  width: 100%;
+
+  header {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 48px;
+  }
+`;
+
+export const Link = styled(RouterLink)`
+  align-items: center;
+  color: var(--title-color);
+  display: flex;
+  font-weight: bold;
+  text-decoration: none;
+
+  svg {
+    color: var(--primary-color);
+    margin-right: 16px;
+  }
+`;
+
+export const Form = styled(Unform)`
+  background: #fff;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  margin: 80px auto;
+  max-width: 730px;
+  padding: 64px;
+
+  h1 {
+    font-size: 36px;
+  }
+
+  fieldset {
+    border: 0px;
+    margin-top: 64px;
+    min-inline-size: auto;
+  }
+
+  legend {
+    align-items: center;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 40px;
+    width: 100%;
+
+    h2 {
+      font-size: 24px;
+    }
+
+    span {
+      color: var(--text-color);
+      font-size: 14px;
+      font-weight: normal;
+    }
+  }
+
+  button {
+    align-self: flex-end;
+    background: var(--primary-color);
+    border: 0;
+    border-radius: 8px;
+    color: #fff;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: bold;
+    height: 56px;
+    margin-top: 40px;
+    transition: background-color 0.2s;
+    width: 260px;
+
+    &:hover {
+      background: #2fb86e;
+    }
+  }
+`;
+
+export const Field = styled.div`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  margin-bottom: 24px;
+
+  input[type='text'],
+  input[type='email'],
+  input[type='number'] {
+    background: #f0f0f5;
+    border: 0;
+    border-radius: 8px;
+    color: #6c6c80;
+    flex: 1;
+    font-size: 16px;
+    padding: 16px 24px;
+
+    &::placeholder {
+      color: #a0a0b2;
+    }
+  }
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    background: #f0f0f5;
+    border: 0;
+    border-radius: 8px;
+    color: #6c6c80;
+    flex: 1;
+    font-size: 16px;
+    padding: 16px 24px;
+  }
+
+  label {
+    font-size: 14px;
+    margin-bottom: 8px;
+  }
+
+  :disabled {
+    cursor: not-allowed;
+  }
+`;
+
+export const FieldGroup = styled.div`
+  display: flex;
+  flex: 1;
+
+  ${Field} + ${Field} {
+    margin-left: 24px;
+  }
+`;
+
+export const Items = styled.ul`
+  display: grid;
+  gap: 16px;
+  grid-template-columns: repeat(3, 1fr);
+  list-style: none;
+`;
+
+interface ItemProps {
+  selected: boolean;
+}
+
+export const Item = styled.li<ItemProps>`
+  align-items: center;
+  background: #f5f5f5;
+  border: 2px solid #f5f5f5;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 180px;
+  padding: 32px 24px 16px;
+  text-align: center;
+
+  ${props =>
+    props.selected &&
+    css`
+      background: #e1faec;
+      border: 2px solid #34cb79;
+    `}
+
+  span {
+    align-items: center;
+    color: var(--title-color);
+    display: flex;
+    flex: 1;
+    margin-top: 12px;
+  }
+`;
+
+export const MapContainer = styled.div`
+  > div {
+    border-radius: 8px;
+    height: 350px;
+    margin-bottom: 24px;
+    width: 100%;
+  }
+`;
