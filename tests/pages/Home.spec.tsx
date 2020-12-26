ae3d6017cccd8cb0a25781b65fc4399495b9c0b5
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-26 17:51:23 -0300

    Test home page

---
+++ tests/pages/Home.spec.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
+import Home from '../../src/pages/Home';
+
+describe('Home page', () => {
+  it('should be able to navigate to register page', async () => {
+    const history = createBrowserHistory();
+    const { getByTestId } = render(
+      <Router history={history}>
+        <Home />
+      </Router>,
+    );
+
+    fireEvent.click(getByTestId('register'));
+
+    expect(history.location.pathname).toBe('/register');
+  });
+});
