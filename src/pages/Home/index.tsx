df81e43945bf0133d80fcfcbe630b77a4f560cca
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-26 17:41:11 -0300

    Remove layout component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,34 +3,30 @@ import { FiLogIn } from 'react-icons/fi';
 
 import Logo from '../../assets/logo.svg';
 import { Container, Main, Link } from './styles';
-import Layout from '../../components/Layout';
 
 const Home: React.FC = () => {
   return (
-    <Layout>
-      <Container>
-        <div>
-          <header>
-            <img src={Logo} alt="Ecoleta" />
-          </header>
+    <Container>
+      <div>
+        <header>
+          <img src={Logo} alt="Ecoleta" />
+        </header>
 
-          <Main>
-            <h1>Seu marketplace de coleta de resíduos.</h1>
-            <p>
-              Ajudamos pessoas a encontrarem pontos de coleta de forma
-              eficiente.
-            </p>
+        <Main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>
+            Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
+          </p>
 
-            <Link to="/register">
-              <span>
-                <FiLogIn />
-              </span>
-              <strong>Cadastrar um ponto de coleta</strong>
-            </Link>
-          </Main>
-        </div>
-      </Container>
-    </Layout>
+          <Link to="/register" data-testid="register">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastrar um ponto de coleta</strong>
+          </Link>
+        </Main>
+      </div>
+    </Container>
   );
 };
 
