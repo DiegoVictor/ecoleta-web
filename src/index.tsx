42794417b2f281936f30068dae0eacc33130acc1
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-08-04 09:58:48 -0300

    Get or create root element if needed and use StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root =
+  document.getElementById('root') ??
+  (() => {
+    const e = document.createElement('div');
+    document.body.append(e);
+
+    return e;
+  })();
+
+createRoot(root).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+  </StrictMode>,
 );
