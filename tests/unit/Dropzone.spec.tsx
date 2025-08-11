d13d2058b2a112b17836960cbbe5610be0bf68cb
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-08-11 09:23:29 -0300

    Update dropzone component tests

--- tests/unit/Dropzone.spec.tsx
+++ tests/unit/Dropzone.spec.tsx
@@ -1,82 +1,64 @@
 import React from 'react';
 import { render, fireEvent, act } from '@testing-library/react';
-import faker from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 
 import { Dropzone } from '../../src/components/Dropzone';
 
-interface DataTransfer {
-  dataTransfer: {
-    files: File[];
-    items: {
-      kind: string;
-      type: string;
-      getAsFile(): File;
-    }[];
-    types: string[];
+const mockUseDropzone = jest.fn();
+jest.mock('react-dropzone', () => {
+  return {
+    useDropzone: (params: Record<string, any>) => mockUseDropzone(params),
   };
-}
+});
 
-const imageUrl = faker.image.imageUrl();
+const imageUrl = faker.image.url();
 global.URL.createObjectURL = jest.fn(() => imageUrl);
 
 describe('Upload', () => {
-  function flushPromises(
-    ui: JSX.Element,
-    container: HTMLElement,
-  ): Promise<HTMLElement> {
-    return new Promise(resolve =>
-      global.setImmediate(() => {
-        render(ui, { container });
-        resolve(container);
-      }),
-    );
-  }
-
-  async function dispatchEvt(
-    node: HTMLDivElement,
-    type: string,
-    data: DataTransfer,
-  ): Promise<void> {
-    const event = new Event(type, { bubbles: true });
-    Object.assign(event, data);
-    await act(async () => {
-      fireEvent(node, event);
-    });
-  }
-
-  function mockData(files: File[]): DataTransfer {
-    return {
-      dataTransfer: {
-        files,
-        items: files.map(file => ({
-          kind: 'file',
-          type: file.type,
-          getAsFile: () => file,
-        })),
-        types: ['Files'],
-      },
-    };
-  }
-
   it('should be able to show a message when dragging a file', async () => {
     const file = new File(['image'], 'ping.jpg', {
       type: 'image/jpeg',
     });
-    const data = mockData([file]);
+    const data = {
+      dataTransfer: {
+        files: [file],
+        items: [file].map(f => ({
+          kind: 'file',
+          type: f.type,
+          getAsFile: () => f,
+        })),
+        types: ['Files'],
+      },
+    };
     const onUpload = jest.fn();
 
-    const component = <DropZone onFileSelected={onUpload} />;
-    const { container, getByText, getByAltText } = render(component);
-    const dropzone = container.querySelector('div');
+    mockUseDropzone.mockImplementation(({ onDrop }) => {
+      return {
+        getRootProps: () => ({
+          onDragEnter: () => {
+            onDrop([file]);
+          },
+        }),
+        getInputProps: jest.fn(),
+      };
+    });
+
+    const { container, getByText, getByAltText } = render(
+      <Dropzone onFileSelected={onUpload} />,
+    );
 
     expect(getByText('Imagem do estabelecimento')).toBeInTheDocument();
+    expect(mockUseDropzone).toHaveBeenCalledWith({
+      onDrop: expect.any(Function),
+      accept: { 'image/*': [] },
+    });
 
-    if (dropzone) {
-      await act(async () => {
-        dispatchEvt(dropzone, 'drop', data);
-      });
-      await flushPromises(component, container);
+    const dropzone = container.querySelector('div');
+    if (!dropzone) {
+      throw new Error('Dropzone not found');
     }
+
+    await act(() => fireEvent.dragEnter(dropzone, data));
 
     expect(onUpload).toHaveBeenCalledWith(file);
     expect(getByAltText('Preview')).toHaveAttribute('src', imageUrl);
