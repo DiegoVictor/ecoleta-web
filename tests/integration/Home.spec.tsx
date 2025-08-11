83add9b99585c49c5cad69135ee68d689b6376d7
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-08-11 09:24:03 -0300

    Refactor Home page tests to use BrowserRouter for navigation

--- tests/integration/Home.spec.tsx
+++ tests/integration/Home.spec.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter, Route, Routes } from 'react-router';
 
 import { Home } from '../../src/pages/Home';
 
 describe('Home page', () => {
   it('should be able to navigate to register page', async () => {
-    const history = createBrowserHistory();
-    const { getByTestId } = render(
-      <Router history={history}>
-        <Home />
-      </Router>,
+    const { getByTestId, getByText } = render(
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Home />} index />
+          <Route path="/register" element={<div>Register</div>} />
+        </Routes>
+      </BrowserRouter>,
     );
 
     fireEvent.click(getByTestId('register'));
 
-    expect(history.location.pathname).toBe('/register');
+    expect(getByText('Register')).toBeInTheDocument();
   });
 });
